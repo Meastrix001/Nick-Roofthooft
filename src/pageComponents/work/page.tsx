91ca7326c0a5113980d4481a125049f1d49379a4
Nick Roofthooft <nroofthooft2012@gmail.com>
2025-10-25 19:03:30 +0300

    Fixes

--- src/pageComponents/work/page.tsx
+++ src/pageComponents/work/page.tsx
@@ -1,16 +1,13 @@
-"use client"
+"use client";
 import { Box, Container, Heading, Section, Text } from "@radix-ui/themes";
 import { WorkList } from "./WorkList/WorkList";
 import { useRouter } from "next/navigation";
 
-
-
 export default function WorkPage() {
-    const router = useRouter()
+    const router = useRouter();
     return (
         <Container px={"4"} mt={"0"} width={{ lg: "100vw" }} className="contact">
-            <Box
-            >
+            <Box>
                 <Heading
                     className="cursor"
                     align={"center"}
@@ -22,15 +19,20 @@ export default function WorkPage() {
                 </Heading>
             </Box>
 
-
             <Box>
-                <Heading as="h2" size={"8"}>Discover my work</Heading>
-                <Text color="gray">List of all my freelance work</Text>
+                <Heading as="h2" size={"8"}>
+                    Discover my work
+                </Heading>
+                <Text color="gray"></Text>
+                <Text color="gray">7 years of development experience</Text>{" "}
+                <Text color="gray">
+                    Currently have Full-time employed for the last 3 years.
+                </Text>
+                <Text color="gray">List of all my freelance work.</Text>
                 <Section>
                     <WorkList />
                 </Section>
             </Box>
-
         </Container>
     );
 }
